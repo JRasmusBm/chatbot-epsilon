703fced489fd8f50f2bf6ce5a0fb2140bfe2598a
jrasmusbm <JRasmusBm@gmail.com>
2019-11-26 09:37:48 +0100

    Finish first version of chatbot ui

--- src/client/app.py
+++ src/client/app.py
@@ -1,8 +1,18 @@
 import os
+from random import choice
 
 from flask import Flask, render_template, request
 
 messages = []
+
+
+def start_chatbot():
+    prompts = [
+        "Hi, how are you?",
+        "What is your favorite color?",
+        "Do you like the winter?",
+    ]
+    return choice(prompts)
 
 
 def create_app(test_config=None):
@@ -22,12 +32,20 @@ def create_app(test_config=None):
 
     # a simple page that says hello
     @app.route("/", methods=["GET", "POST"])
-    def epsilon():
+    def epsilon():  # pylint: disable=unused-variable
+        global messages
         if request.method == "POST":
             messages.append(dict(sender="me", text=request.form["message"]))
             messages.append(
-                dict(sender="them", text=request.form["message"][::-1])
+                dict(
+                    sender="them",
+                    text=" ".join(request.form["message"].split(" ")[::-1]),
+                )
             )
+        elif request.method == "GET":
+            message = start_chatbot()
+            messages = [dict(sender="them", text=message)]
+
         return render_template("epsilon.html", messages=messages)
 
     return app
