25629c8f7762a3140b3d5c5f49efafaff2fe202e
jrasmusbm <JRasmusBm@gmail.com>
2020-01-08 10:28:18 +0100

    Add git freeze temp file

--- src/client/app.py
+++ src/client/app.py
@@ -23,7 +23,7 @@ def new_prompt():
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY="dev")
-    sentiment = Sentiment(f"{trained_models_folder}/imdb_early_stopping.pt")
+    sentiment = Sentiment(f"{trained_models_folder}/temp.pt")
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
